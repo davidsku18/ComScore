2a40c56fe6ceff748b55ff6b3a272d9b88128e85
davidsku18 <davidsku18@up.edu>
2019-05-07 14:58:48 -0700

    Implemented dataframe to database
    Used pandas to_sql function to convert the dataframe object to db file so that queries can be done on it.

--- parseData.py
+++ parseData.py
@@ -1,4 +1,20 @@
-import pandas
+import pandas as pd
+import sqlite3
 
-df = pandas.read_csv('sampleData.csv', sep='|', index_col='STB')
-print(df)
+filepath = 'data.csv'
+
+def create_table():
+    # Parses csv to dataframe object
+    df = pd.read_csv('sampleData.csv', sep='|')
+
+    # Connecting to the sqlite3
+    conn = sqlite3.connect('movie.db')
+    c = conn.cursor()
+
+    # Dataframe object to database
+    df.to_sql(name = 'movie', con = conn, if_exists = 'replace')
+
+
+def show_all():
+    create_table()
+    c.execute("SELECT * FROM movie").fetchall()
