6fc46f388c99a7fe8254fa65c9da2a36c0ea01ad
davidsku18 <davidsku18@up.edu>
2019-05-11 17:26:50 -0700

    Implemented decimal and unique records
    Round REV to 2 decimal places to account for cents. Made records unqiue by STB, TITLE, and DATE

--- parseData.py
+++ parseData.py
@@ -5,15 +5,22 @@ import sqlalchemy
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "movie.db")
 
-# Parses csv to dataframe object
-df = pd.read_csv('sampleData.csv', sep='|')
-df['VIEW_TIME'] = pd.to_datetime(df['VIEW_TIME'], format='%H:%M').dt.time
-df['DATE'] = pd.to_datetime(df['DATE'], format='%Y-%m-%d').dt.date
-
-# Connecting to the sqlite3
+# Connecting to sqlite3
 engine = sqlalchemy.create_engine('sqlite:///' + db_path)
 
-# Dataframe object to database
-df.to_sql(name = 'movie', con = engine, if_exists = 'replace')
+# Parses csv file and creates table
+def create_table():
+    # Parses csv to dataframe object
+    df = pd.read_csv('sampleData.csv', sep='|')
 
-engine.execute("SELECT * FROM movie")
+    # Formats the columns into their respective data types
+    df['DATE'] = pd.to_datetime(df['DATE'], format='%Y-%m-%d').dt.date
+    df['VIEW_TIME'] = pd.to_datetime(df['VIEW_TIME'], format='%H:%M').dt.time
+    # Round REV up to 2 decimal places for cents
+    df['REV'] = df['REV'].round(decimals=2)
+
+    # Sets records to be unique by STB, TITLE, and DATE
+    df = df.drop_duplicates(subset=['STB', 'TITLE', 'DATE'])
+
+    # Dataframe object to sql database, if exists then overwrite
+    df.to_sql(name='movie', con=engine, if_exists='replace')
