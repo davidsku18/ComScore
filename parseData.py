b0afa10dcd4ce8008cc21d07c020110f9fcab2cd
davidsku18 <davidsku18@up.edu>
2019-05-12 02:11:57 -0700

    Implemented SELECT, FILTER, and ORDER
    Utilized argparse to query the data with -s (SELECT), -f (FILTER), -o (ORDER)

--- parseData.py
+++ parseData.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import os.path
 import sqlite3
+import argparse
 
 # Connecting to sqlite3 and storing into main memory
 conn = sqlite3.connect(':memory:')
-cur = conn.cursor()
 
 # Parses csv file and creates table
 def create_table():
-    buffer = ''
     # Parses csv to dataframe object
     df = pd.read_csv('sampleData.csv', sep='|')
 
@@ -23,3 +22,45 @@ def create_table():
 
     # Dataframe object to sql database, if exists then overwrite
     df.to_sql(name='movie', con=conn, if_exists='replace')
+
+create_table()
+parser = argparse.ArgumentParser(description='Process queries')
+parser.add_argument("-s", "--SELECT", nargs='+',
+            help="Selects the specified column")
+parser.add_argument("-o", "--ORDER", nargs='+',
+            help="The column(s) that the data will be ordered with")
+parser.add_argument("-f", "--FILTER", nargs='+',
+            help="Finds the specified data")
+args = parser.parse_args()
+
+# Gets the SELECT arguments
+def get_select():
+    if(args.SELECT is not None):
+        select = ', '.join(args.SELECT)
+        select = 'SELECT ' + select
+        return select
+    else:
+        select = 'SELECT *'
+        return select
+
+# Gets the ORDER arguments
+def get_order():
+    if(args.ORDER is not None):
+        order = ', '.join(args.ORDER)
+        order = 'ORDER BY ' + order
+        return order
+    else:
+        order = ''
+        return order
+
+# Gets the FILTER arguments
+def get_filter():
+    if(args.FILTER is not None):
+        filter = filter = ''.join(args.FILTER).split('=')
+        filter = 'WHERE ' + filter[0] + ' = ' + "'" + filter[1] + "'"
+        return filter
+    else:
+        filter = ''
+        return filter
+
+print(pd.read_sql_query(sql=get_select() + ' FROM movie ' + get_filter() + get_order() + ";", con=conn))
